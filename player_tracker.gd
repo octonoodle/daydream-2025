2ad8f9ee11a2f5ae18db34505f981f54c0118a2d
DeepFryer123 <nathanc5501@gmail.com>
2025-09-27 10:22:06 -0400

    Stability
    Added a variable to track shakiness

--- player_tracker.gd
+++ player_tracker.gd
@@ -5,7 +5,7 @@ var fingers: int = 5            # Number of fingers (0–5)
 var vision: float = 100.0       # Vision clarity/field of view (0–100)
 var hearing: float = 100.0      # Hearing clarity (0–100)
 var mobility: float = 100.0     # Movement ability (0–100)
-
+var stability: float = 0.0
 # Called when the node enters the scene tree
 func _ready():
 	print_status()
@@ -29,6 +29,10 @@ func reduce_mobility(amount: float):
 	mobility = max(mobility - amount, 0)
 	print("Joint pain! Current mobility:", mobility)
 
+func reduce_stability(amount: float):
+	stability = min(stability+amount,100)
+	print("What'd you take? Current Shakiness:", stability)
+	
 # -------------------------
 # Check Status
 # -------------------------
@@ -40,4 +44,5 @@ func print_status():
 	print("Player Status → Fingers:", fingers,
 		  " Vision:", vision,
 		  " Hearing:", hearing,
-		  " Mobility:", mobility)
+		  " Mobility:", mobility,
+		  " Shakiness:", stability)
